93817f5e925a15d29b328b2189d3a5dfb987033c
Gayatri Nayak <gayatree999@gmail.com>
2024-02-28 11:51:13 +0530

    updated comment in standalone file

--- src/test/java/rahulshettyacademy/tests/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandAloneTest.java
@@ -19,6 +19,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//New comments added
 
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
